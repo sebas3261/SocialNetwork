b9eeafcdea678e837c5a5c71bbf66018324bdb9d
sebas3261 <sebassanchez3261@gmail.com>
2024-09-03 16:52:02 -0700

    lista

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,55 @@
 import {View, Text} from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+type Pokemon ={
+    url: string,
+    name: string
+}
 
 export default function Page(){
+
+    const [listPokemon, setListPokemons] = useState([]as Pokemon[]);
+
+    useEffect(()=>{
+        callApi();
+    },[]);
+
+    useEffect(()=>{
+        console.log({listPokemon})
+    },[listPokemon])
+
+    const callApi = async()=>{
+        try{
+            const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+            const data = await response.json()
+            console.log(
+                response,
+                data
+            )
+            setListPokemons(data.results as Pokemon[]);
+        }
+        catch(error){
+            console.log("error: ",error)
+        }
+    }
+     
     return(
         <View>
-            <Text>HOLA Mundo</Text>
-            
+            {
+                listPokemon.map((pokemon, index)=> (
+                    <View
+                        style={{
+                            borderWidth: 1,
+                            borderBlockColor: 'black',
+                            padding: 10,
+                            borderRadius: 10,
+                            marginBottom: 10,
+                        }}
+                    >
+                        <Text key={index}>{pokemon.name}</Text>
+                    </View>
+                ))
+            }
         </View>
     )
 }
