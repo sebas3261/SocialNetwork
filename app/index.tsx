634fa13134c3511b036d8b678101f39e2fbc5e52
sebas3261 <sebassanchez3261@gmail.com>
2024-09-03 15:52:00 -0700

    subir app

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+export default function Page(){
+    return(
+        <View>
+            <Text>HOLA Mundo</Text>
+            
+        </View>
+    )
+}
